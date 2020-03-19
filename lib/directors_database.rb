1539a9d8ee39fdcc889fe40058d3c237e43959c2
Austen Bright <pureaustenbright@gmail.com>
2020-03-19 23:52:48 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,6 +1,13 @@
 require 'yaml'
-
+require 'pry'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+# movies = directors_database[0][:movies]
+# index = 0
+# while index < movies.length
+#
+# 	binding.pry
+# 	index += 1
+# end
